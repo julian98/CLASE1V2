a029a8511d194b9d529212bd52812bad331e2a87
JULIAN ARTURO GOMEZ ACEVEDO <41615045@ort>
2016-04-19 14:36:35 -0300

    Initial commit

---
+++ app/src/main/java/com/example/clase/clase1v2/printActivity.java
@@ -0,0 +1,42 @@
+package com.example.clase.clase1v2;
+
+        import android.os.Bundle;
+        import android.support.v7.app.AppCompatActivity;
+        import android.widget.TextView;
+        import android.widget.Toast;
+
+
+public class printActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_print);
+
+        Bundle extras = getIntent().getExtras();
+        if (extras == null) {
+            return;
+        }
+
+        Persona p = (Persona)extras.getSerializable("pers1");
+        Persona pppp = (Persona)extras.getSerializable("pers2");
+
+        TextView nombreyapVw = (TextView)findViewById(R.id.nombreyapellido);
+        TextView sexoVw = (TextView)findViewById(R.id.sexo);
+
+        try {
+            nombreyapVw.setText(p.imprimir());
+        } catch (Exception e) {
+            Toast.makeText(this,e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+
+        //String sexoStr = String.valueOf(p.getSexo());
+        String sexoStr;
+        if (p.getSexo() == Persona.FEMENINO)
+            sexoStr = "Femenino";
+        else
+            sexoStr = "Masculino";
+        sexoVw.setText(sexoStr);
+
+    }
+}
